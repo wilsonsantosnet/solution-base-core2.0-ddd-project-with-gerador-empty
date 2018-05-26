c9c364da1eff26bf5e1fc69aa6640961c504224b
Wilson Santos <wilsonsantosnet@gmail.com>
2018-05-26 19:14:51 -0300

    ++

--- seed.CrossCuting.Auth/ProfileCustom.cs
+++ seed.CrossCuting.Auth/ProfileCustom.cs
@@ -1,4 +1,4 @@
-﻿using Common.Domain.Model;
+using Common.Domain.Model;
 using IdentityModel;
 using Newtonsoft.Json;
 using System;
@@ -9,51 +9,57 @@ using System.Text;
 
 namespace Seed.CrossCuting
 {
-    public class ProfileCustom 
+    public class ProfileCustom
     {
 
-        public static Dictionary<string, object> Claims(Dictionary<string, object>  _claims)
+        enum Role
         {
+            Admin = 1,
+            Tenant = 2,
+            Owner = 3,
+        }
+
+        public static Dictionary<string, object> Claims(Dictionary<string, object> _claims)
+        {
+
+            var role = GetRole(_claims);
+
+            if (role.ToLower() == Role.Admin.ToString().ToLower())
+                _claims.AddRange(ClaimsForAdmin());
+
             return _claims;
         }
 
-
-        public static List<Claim> ClaimsForAdmin(string name, string email)
+        private static string GetRole(Dictionary<string, object> _claims)
         {
+            return _claims.Where(_ => _.Key == "role").SingleOrDefault().Value.ToString();
+        }
 
+        public static Dictionary<string, object> ClaimsForAdmin()
+        {
             var tools = new List<dynamic>
             {
-                new { Icon = "fa fa-edit", Name = "BancoBrasilChave", Value = "/bancobrasilchave" },
-
+                new { Icon = "fa fa-edit", Name = "Tool", Route = "#/Url" },
             };
-
             var _toolsForAdmin = JsonConvert.SerializeObject(tools);
-
-            return new List<Claim>
+            return new Dictionary<string, object>
             {
-                new Claim(JwtClaimTypes.Name, name),
-                new Claim(JwtClaimTypes.Email, email),
-                new Claim("tools", _toolsForAdmin),
-                new Claim("role", "admin"),
+                { "tools", _toolsForAdmin }
             };
         }
 
-        public static List<Claim> ClaimsForTenant(int tenantId, string name, string email)
+        public static Dictionary<string, object> ClaimsForTenant(int tenantId, string name, string email)
         {
 
             var tools = new List<dynamic>
             {
-                new { Icon = "fa fa-edit", Name = "Tool", Value = "#/Url" },
+                new { Icon = "fa fa-edit", Name = "Tool", Route = "#/Url" },
             };
 
             var _toolsForSubscriber = JsonConvert.SerializeObject(tools);
-
-            return new List<Claim>
+            return new Dictionary<string, object>
             {
-                new Claim(JwtClaimTypes.Name, name),
-                new Claim(JwtClaimTypes.Email, email),
-                new Claim("tools",_toolsForSubscriber),
-                new Claim("role","tenant"),
+                { "tools", _toolsForSubscriber }
             };
         }
 

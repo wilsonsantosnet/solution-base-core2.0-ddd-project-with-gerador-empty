d4a052b28e2eefd559d8adf6ff252d1f34c9a2ef
Wilson Santos <wilsonsantosnet@gmail.com>
2018-08-20 16:29:01 -0300

    ++

---
+++ Common.Domain/Enums/EOperation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Domain.Enums
+{
+    public enum EOperationAccess
+    {
+        CanRead, 
+        CanWrite, 
+        CanDelete,
+    }
+}
---
+++ Common.Domain/Enums/ETypeTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Domain.Enums
+{
+    public enum ETypeTools
+    {
+
+        Menu = 1,
+        Feature = 2,
+    }
+}

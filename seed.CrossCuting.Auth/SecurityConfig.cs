7dec92def18d9d770d5ce4f095af85dfaea3cf32
Wilson Santos <wilsonsantosnet@gmail.com>
2018-09-25 16:34:10 -0300

    ++

---
+++ seed.CrossCuting.Auth/SecurityConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Score.Platform.Account.CrossCuting.Auth
+{
+    public static class SecurityConfig
+    {
+        public static string GetSalt()
+        {
+            return "ScorePlatform321$";
+        }
+
+    }
+}

c421ce2b9b7fa8a8c6cf12dc16be5843a0785a8b
Wilson Santos <wilsonsantosnet@gmail.com>
2018-09-28 09:11:41 -0300

    ++

---
+++ seed.CrossCuting.Auth/SecurityConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seed.CrossCuting.Auth
+{
+    public static class SecurityConfig
+    {
+        public static string GetSalt()
+        {
+            return "Seed321$";
+        }
+
+    }
+}

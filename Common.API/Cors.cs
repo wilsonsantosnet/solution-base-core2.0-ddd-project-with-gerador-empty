ec84b6079ca85a50e823fd2cf07473f83d7fb9d2
Wilson Santos <wilsonsantosnet@gmail.com>
2018-10-11 15:57:48 -0300

    add AllowStackOrigin

--- Common.API/Cors.cs
+++ Common.API/Cors.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Cors.Internal;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,13 +7,22 @@ namespace Common.API
     public static class Cors
     {
 
-        public static void Enable(IServiceCollection services)
+        public static void Enable(IServiceCollection services, params string[] origins)
         {
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowAnyOrigin",
                     builder => builder
                     .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowStackOrigin",
+                    builder => builder
+                    .WithOrigins(origins)
                     .AllowAnyHeader()
                     .AllowAnyMethod());
             });

55afd43f2e59f42c80014a2aab3599b1c8638f76
Wilson Santos <wilsonsantosnet@gmail.com>
2020-04-20 18:52:43 -0300

    add ConfigContextCustom

--- Gerador.Gen/ConfigContext.cs
+++ Gerador.Gen/ConfigContext.cs
@@ -1,10 +1,6 @@
 using Common.Gen;
-using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Seed.Gen
 {
@@ -48,7 +44,7 @@ namespace Seed.Gen
                 Routes = new List<RouteConfig> {
                     new RouteConfig{ Route = "{ path: 'sampledash',  canActivate: [AuthGuard], loadChildren: () => import('./main/sampledash/sampledash.module').then(m => m.SampleDashModule) }" }
                 },
-                
+
                 TableInfo = new UniqueListTableInfo
                 {
                    new TableInfo().FromTable("Sample").MakeBack().MakeFront().AndConfigureThisFields(new List<FieldConfig> {
@@ -65,15 +61,58 @@ namespace Seed.Gen
         }
 
 
+        private Context ConfigContextCustom()
+        {
+            var contextName = "Seed";
+
+            return new Context
+            {
+
+                ConnectionString = ConfigurationManager.ConnectionStrings["Seed"].ConnectionString,
+
+                Namespace = "Seed",
+                ContextName = contextName,
+                ShowKeysInFront = false,
+                LengthBigField = 250,
+                OverrideFiles = true,
+                UseRouteGuardInFront = true,
+
+                OutputClassDomain = ConfigurationManager.AppSettings[string.Format("outputClassDomain")],
+                OutputClassInfra = ConfigurationManager.AppSettings[string.Format("outputClassInfra")],
+                OutputClassDto = ConfigurationManager.AppSettings[string.Format("outputClassDto")],
+                OutputClassApp = ConfigurationManager.AppSettings[string.Format("outputClassApp")],
+                OutputClassApi = ConfigurationManager.AppSettings[string.Format("outputClassApi")],
+                OutputClassFilter = ConfigurationManager.AppSettings[string.Format("outputClassFilter")],
+                OutputClassSummary = ConfigurationManager.AppSettings[string.Format("outputClassSummary")],
+                OutputAngular = ConfigurationManager.AppSettings["OutputAngularCustom"],
+                OutputClassSso = ConfigurationManager.AppSettings["OutputClassSso"],
+                OutputClassCrossCustingAuth = ConfigurationManager.AppSettings["OutputClassCrossCustingAuth"],
+
+                Arquiteture = ArquitetureType.DDD,
+                CamelCasing = true,
+                MakeFront = true,
+                AlertNotFoundTable = true,
+                MakeToolsProfile = true,
+
+                Routes = new List<RouteConfig> {
+                    new RouteConfig{ Route = "{ path: 'sampledash',  canActivate: [AuthGuard], loadChildren: () => import('./main/sampledash/sampledash.module').then(m => m.SampleDashModule) }" }
+                },
+
+                TableInfo = new UniqueListTableInfo
+                {
+                   new TableInfo().FromClass("SampleDash").MakeFrontBasic(),
+                }
+            };
+        }
+
+
 
         public IEnumerable<Context> GetConfigContext()
         {
-
             return new List<Context>
             {
-
                 ConfigContextDefault(),
-
+                ConfigContextCustom(),
             };
 
         }

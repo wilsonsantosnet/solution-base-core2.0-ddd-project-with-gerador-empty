524ef62772e7abd4990bb0f05d533fe4c71dad61
Wilson Santos <wilsonsantosnet@gmail.com>
2020-04-23 00:34:15 -0300

    add Client Credencial Customization

---
+++ Sso.Server.Api/ClientCredencialRequestValidator.cs
@@ -0,0 +1,32 @@
+﻿using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Sso.Server.Api
+{
+    public class ClientCredentialRequestValidator : ICustomTokenRequestValidator
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public ClientCredentialRequestValidator(IHttpContextAccessor httpContextAccessor)
+        {
+            this._httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task ValidateAsync(CustomTokenRequestValidationContext context)
+        {
+            var client = context.Result.ValidatedRequest.Client;
+            var _roles = JsonConvert.SerializeObject(new List<string> { "anonymous" });
+
+            var claims = new List<Claim> {
+                new Claim("role", _roles),
+                new Claim("typerole", "anonymous"),
+            };
+            claims.ToList().ForEach(u => context.Result.ValidatedRequest.ClientClaims.Add(u));
+            context.Result.ValidatedRequest.Client.ClientClaimsPrefix = "";
+        }
+    }
+}

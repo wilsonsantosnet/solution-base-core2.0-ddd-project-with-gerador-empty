c7d420a82280d7fcb5b825f13740aa807654c7e1
Wilson Santos <wilsonsantosnet@gmail.com>
2020-05-22 10:57:34 -0300

    fix references

--- Seed.CrossCuting/ExportExcelCustom.cs
+++ Seed.CrossCuting/ExportExcelCustom.cs
@@ -5,11 +5,6 @@ namespace Seed.CrossCuting
 {
     public class ExportExcelCustom<T> : ExportExcel<T>
     {
-        public ExportExcelCustom(FilterBase filter) : base(filter)
-        {
-
-
-
-        }
+       
     }
 }

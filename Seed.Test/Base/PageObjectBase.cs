522a392fc8437323e58f74ec5b85b12bcf6b8285
Wilson Santos <wilsonsantosnet@gmail.com>
2020-05-22 07:44:08 -0300

    add test project

---
+++ Seed.Test/Base/PageObjectBase.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+
+namespace Seed.Test.Base
+{
+    public abstract class PageObjectBase
+    {
+
+        protected string GetSection(string section)
+        {
+            var config = LoadFile();
+            return config.GetSection(section).Value;
+        }
+
+        protected IOptions<T> GetSection<T>(string section) where T : class, new()
+        {
+            var config = LoadFile();
+            var newInstance = Activator.CreateInstance(typeof(T)) as T; 
+            config.GetSection(section).Bind(newInstance);
+            var result = Options.Create(newInstance);
+            return result;
+        }
+
+        protected  Mock<ILoggerFactory> MakeLogFactory<T>()
+        {
+            var mockLogger = new Mock<ILogger<T>>();
+            mockLogger.Setup(
+                m => m.Log(
+                    LogLevel.Information,
+                    It.IsAny<EventId>(),
+                    It.IsAny<object>(),
+                    It.IsAny<Exception>(),
+                    It.IsAny<Func<object, Exception, string>>()));
+
+            var mockLoggerFactory = new Mock<ILoggerFactory>();
+            mockLoggerFactory.Setup(x => x.CreateLogger(It.IsAny<string>())).Returns(() => mockLogger.Object);
+            return mockLoggerFactory;
+        }
+
+        private static IConfigurationRoot LoadFile()
+        {
+            return new ConfigurationBuilder()
+                          .SetBasePath(AppContext.BaseDirectory)
+                          .AddJsonFile("appsettings.json", false, true)
+                          .Build();
+        }
+    }
+}
---
+++ Seed.Test/Integration/IntegrationTestSample.cs
@@ -0,0 +1,16 @@
+﻿using Seed.Test.Base;
+using Xunit;
+
+namespace Seed.Test.Integration
+{
+    public class IntegrationTestSample : PageObjectBase
+    {
+        [Fact]
+        public void Test1() { 
+
+
+        
+        }
+
+    }
+}

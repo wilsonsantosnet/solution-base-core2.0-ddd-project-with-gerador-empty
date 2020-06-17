72f489b18489c0f13c40f36dba09abff626745b0
Wilson Santos <wilsonsantosnet@gmail.com>
2020-06-17 14:19:58 -0300

    add SchedulesContainer

--- Seed.HangFire/Config/ConfigContainer.ext.cs
+++ Seed.HangFire/Config/ConfigContainer.ext.cs
@@ -1,9 +1,12 @@
-﻿using Common.Api;
+using Common.Api;
 using Common.Cripto;
 using Common.Domain;
 using Common.Domain.Interfaces;
 using Common.Domain.Model;
+using HangFire.Model;
 using Microsoft.Extensions.DependencyInjection;
+using Seed.HangFire.Base;
+using Seed.HangFire.Interfaces;
 
 namespace Seed.HangFire.Config
 {
@@ -14,6 +17,8 @@ namespace Seed.HangFire.Config
             services.AddScoped<CurrentUser>();
             services.AddScoped<IRequest, Request>();
             services.AddScoped<ICripto, Cripto>();
+            services.AddScoped<ISchedulesContainer, SchedulesContainer>();
+            services.AddSingleton<JobExecutionControl>();
         }
     }
 }

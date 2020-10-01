d486f75e48e7da5d72786c807c714496203708e1
Wilson Santos <wilsonsantosnet@gmail.com>
2020-10-01 08:46:12 -0300

    namespace fix

--- Sso.Server.Api/Startup.cs
+++ Sso.Server.Api/Startup.cs
@@ -1,9 +1,11 @@
-﻿using Common.API;
+using Common.API;
 using Common.Domain.Base;
 using Common.Domain.Model;
 using IdentityServer4.Validation;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.CookiePolicy;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
